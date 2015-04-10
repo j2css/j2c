9011159bc7d56ee8739249cc8b70c560630dcf46
Pierre-Yves Gerardy <py@py.gy>
2015-04-10 19:30:28 +0200

    Sync API with docs. Add license

--- j2c.js
+++ j2c.js
@@ -1,4 +1,4 @@
-(function () {
+module.exports = (function () {
   var
     OBJECT = "[object Object]",
     NUMBER = "[object Number]",
@@ -15,7 +15,7 @@
     buf.push(indent + prop)
   }
 
-  function properties(o) {
+  function inline(o) {
     var buf = [];
     _properties(o, buf, "", "");
     return buf.join("\n");
@@ -101,7 +101,7 @@
   };
 
   var m = {
-    properties: properties,
+    inline: inline,
     vendors:["o", "ms", "moz", "webkit"],
     unit: "px",
     prefix:".Dimrill_" + Date.now() + "_",
@@ -111,3 +111,25 @@
 
   return m;
 })();
+
+/*
+Copyright © 2015 Pierre-Yves Gérardy
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the “Software”),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+*/
