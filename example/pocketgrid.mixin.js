a521e586e433dcf393501de0a64a73d348786dfb
Pierre-Yves Gerardy <py@py.gy>
2015-04-21 01:14:40 +0200

    Fix the pocketgrid mixin

--- example/pocketgrid.mixin.js
+++ example/pocketgrid.mixin.js
@@ -1,38 +1,38 @@
 // If you wanted to turn PockeGrid into a mixin, you could use this:
 
 var pocketgrid = {
-  blockgroup: [
-    ",:before,:after":{ //note the initial coma
-      "box-sizing":"border-box"
-    },
-    /* Clearfix */
-    "*zoom: 1",
-    {
-      ":before,:after": {
-        display: "table",
-        content: '""',
-        "line-heigth": 0
-      },
-      ":after": {clear:"both"},
+    blockgroup: [
+        /* Clearfix */
+        "*zoom: 1",
+        {
+            ",:before,:after":{ //note the initial coma
+                "box-sizing":"border-box"
+            },
+            ":before,:after": {
+                display: "table",
+                content: '""',
+                "line-heigth": 0
+            },
+            ":after": {clear:"both"},
 
-      /* ul/li compatibility */
-      "list-style-type":"none",
-      padding:0,
-      margin:0,
+            /* ul/li compatibility */
+            "list-style-type":"none",
+            padding:0,
+            margin:0,
 
-      " > .blockgroup": {
-        /* Nested grid */
-        clear: "none",
+            " > .blockgroup": {
+                /* Nested grid */
+                clear: "none",
+                float: "left",
+                margin: "0 !important"
+            }
+        }
+    ],
+    block: {
+        ",:before,:after":{ //note the initial coma
+            box_sizing:"border-box"
+        },
         float: "left",
-        margin: "0 !important"
-      }
+        width: "100%"
     }
-  ],
-  block: {
-    ",:before,:after":{ //note the initial coma
-      "box-sizing":"border-box"
-    },
-    float: "left",
-    width: "100%"
-  }
 }
