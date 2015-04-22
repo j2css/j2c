e73d29e97fe2e8eb573ae959b639bff82fa00197
Pierre-Yves Gerardy <py@py.gy>
2015-04-23 00:10:04 +0200

    Add the PocketGrid license

--- example/pocketgrid.js
+++ example/pocketgrid.js
@@ -1,12 +1,30 @@
-/*! PocketGrid 1.1.0
-* Copyright 2013 Arnaud Leray
-* MIT License
+/* 
+
+PocketGrid 1.1.0 Copyright (c) 2013 Arnaud Leray
+j2c port Copyright (c) 2013 Pierre-Yves Gérardy
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
 */
 var j2c = require("../dist/j2c.commonjs");
 
 j2c.vendors = [];
-
-// here's a straight port.
 
 console.log(j2c.sheet("").add({
   /* Border-box-sizing */
