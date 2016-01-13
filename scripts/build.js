d40b84fe779c1aaf53cb9e7e17b9930c9922b17e
Pierre-Yves Gerardy <py@py.gy>
2016-01-13 23:20:09 +0100

    on build error, exit early with a non-zero error code

--- scripts/build.js
+++ scripts/build.js
@@ -63,79 +63,8 @@ outputs.forEach(function (output) {
         }
       })
     }
-  }).then(null, function (e) {console.log(output.name, e)})
-
-//     if (wrappers[wrp].minify) {
-// /*jshint -W083 */
-//         (function(){
-//             var minified = uglify.minify(
-//                 wrapped, {fromString: true}).code,
-//                 _name = name,
-//                 _wrp = wrp;
-
-//             fs.writeFileSync('dist/' + name + '.' + wrp + '.min.js', minified);
-//             zlib.gzip(minified, function(_, buf){
-//                 console.log(_name+'.'+_wrp, _ || buf.length);
-//             });
-//         })();
-// /*jshint +W083 */
-//     }
-
+  }).then(null, function (e) {
+    console.log(output.name, e)
+    process.exit(1)
+  })
 })
-
-
-
-// var fs = require("fs"),
-//     uglify = require("uglify-js"),
-//     zlib = require("zlib"),
-//     surgicate = require("surgicate"),
-//     source = surgicate({path:"j2c.js"}).excise("notice").toString(),
-//     versions = {
-//         "j2c": surgicate({source:source}).excise("inline"),
-//         "inline/j2c": surgicate({source:source}).excise("statements") + ""
-//     },
-//     wrappers = {
-//         global: {
-//             source: "var j2c = %;",
-//             minify: true
-//         },
-//         commonjs: {
-//             source: "module.exports = %;",
-//             minify: true // No need to minify before browserification.
-//         },
-//         es6: {
-//             source: "export default %;",
-//             minify: false // ATM, uglify chokes on the export statement.
-//         },
-//         amd: {
-//             source: "define('j2c', function(){return %});",
-//             minify: true
-//         }
-//     };
-
-// /*jshint -W079 */
-// for (var name in versions) {
-// /*jshint +W079 */
-//     var src = versions[name];
-//     for (var wrp in wrappers) {
-//         var wrapped = wrappers[wrp].source.replace("%", src);
-
-//         fs.writeFileSync("dist/" + name + "." + wrp + ".js", wrapped);
-//         if (wrappers[wrp].minify) {
-// /*jshint -W083 */
-//             (function(){
-//                 var minified = uglify.minify(
-//                     wrapped, {fromString: true}).code,
-//                     _name = name,
-//                     _wrp = wrp;
-
-//                 fs.writeFileSync("dist/" + name + "." + wrp + ".min.js", minified);
-//                 zlib.gzip(minified, function(_, buf){
-//                     console.log(_name+"."+_wrp, _ || buf.length);
-//                 });
-//             })();
-// /*jshint +W083 */
-//         }
-//     }
-// }
-
