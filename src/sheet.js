74c6390848c72f5c0daa4f238740303b5bb4fd6a
Pierre-Yves Gerardy <py@py.gy>
2016-02-09 19:31:05 +0100

    100% coverage at the branch level

--- src/sheet.js
+++ src/sheet.js
@@ -33,19 +33,19 @@ export function sheet(statements, buf, prefix, composes, local, state) {
       if (prefix && /^[-\w$]+$/.test(k)) {
         if (!inDeclaration) {
           inDeclaration = 1
-          buf.s(( prefix || ':-error-no-selector' ), ' {\n')
+          buf.s((prefix), ' {\n')
         }
         declarations(v, buf, k, local, state)
       } else if (/^@/.test(k)) {
         // Handle At-rules
-        inDeclaration = (inDeclaration && buf.c('}\n') && 0)
+        inDeclaration = (inDeclaration && (buf.c('}\n') || 1) && 0)
 
         at(k, v, buf, prefix, composes, local, state)
 
       } else {
         // selector or nested sub-selectors
 
-        inDeclaration = (inDeclaration && buf.c('}\n') && 0)
+        inDeclaration = (inDeclaration && (buf.c('}\n') || 1) && 0)
 
 
         sheet(v, buf,
