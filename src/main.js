a4531ca8e0ac77bdd02fec7baf8a03987f21f534
Pierre-Yves Gerardy <py@py.gy>
2016-03-08 00:41:14 +0100

    Moved public helpers to their own place

--- src/main.js
+++ src/main.js
@@ -2,29 +2,8 @@ import {own, flatIter, emptyArray, type, FUNCTION, OBJECT} from './helpers'
 import {sheet, closeSelectors} from './sheet'
 import {declarations} from './declarations'
 import {atRules} from './at-rules'
+import {at, global, kv} from './extras'
 
-function global(x) {
-  return ':global(' + x + ')'
-}
-
-function kv (k, v, o) {
-  o = {}
-  o[k] = v
-  return o
-}
-
-function at (rule, params, block) {
-  if (
-    arguments.length < 3
-  ) {
-    // inner curry!
-    var _at = at.bind.apply(at, [null].concat([].slice.call(arguments,0)))
-    // So that it can be used as a key in an ES6 object literal.
-    _at.toString = function(){return '@' + rule + ' ' + params}
-    return _at
-  }
-  else return kv('@' + rule +' ' + params, block)
-}
 
 export default function j2c() {
   var $filters = [closeSelectors]
