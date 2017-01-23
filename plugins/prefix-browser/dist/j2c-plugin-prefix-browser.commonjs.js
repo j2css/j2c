c7080182a4784a283b877ba72b6d9f52d583eaab
Pierre-Yves Gerardy <py@py.gy>
2017-01-23 20:36:53 +0100

    [prefix-plugin] don't prefix ::placeholder when it is also supported unprefixed

--- plugins/prefix-browser/dist/j2c-plugin-prefix-browser.commonjs.js
+++ plugins/prefix-browser/dist/j2c-plugin-prefix-browser.commonjs.js
@@ -338,7 +338,7 @@ function detectSelectors(fixers) {
   // builds an array of selectors that need a prefix.
   for (selector in selectors) {
     prefixed = prefixSelector(selector);
-    if(!supportedRule(selector) && supportedRule(prefixed)) {
+    if(!supportedRule(selectors[selector]) && supportedRule(prefixed)) {
       fixers.hasSelectors = true;
       fixers.selectorList.push(selectors[selector]);
       fixers.selectorMap[selectors[selector]] = prefixed;
